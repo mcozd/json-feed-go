b4268ade748d49dcf92b8e3d66470d550ceccd57
Patrick Ostie <patrick.ostie@stroeer.de>
2022-09-09 17:30:38 +0200

    feat: initial version of parsing functionality

---
+++ parser.go
@@ -0,0 +1,27 @@
+package jsonfeed
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+type Parser struct{}
+
+func FromString(jsonStr string) (feed *Feed, err error) {
+	jsonBytes := bytes.NewBufferString(jsonStr).Bytes()
+	return FromBytes(jsonBytes)
+}
+
+func FromBytes(jsonBytes []byte) (feed *Feed, err error) {
+	json.Unmarshal(jsonBytes, &feed)
+	return
+}
+
+func FromReader(reader io.Reader) (feed *Feed, err error) {
+	jsonBytes, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return FromBytes(jsonBytes)
+}
